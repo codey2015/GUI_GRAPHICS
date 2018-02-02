e98ef723216b99a133df03f21d5d502208b646e5
codey2015 <35516651+codey2015@users.noreply.github.com>
2018-02-01 21:11:43 -0800

    Update cube.js

--- Assignment4/cube.js
+++ Assignment4/cube.js
@@ -27,7 +27,7 @@ function render() {
   angle += 5.0; // degrees
 
   //cube.MV = rotate( angle, [1, 1, 0] );
-  cube.rotate(modelViewMatrix, modelViewMatrix, cubeRotation * .7, [0, 1, 0]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation * .7, [0, 1, 0]);
   cube.render();
 
   requestAnimationFrame( render ); // schedule another call to render()
