23a9c59fad37133e162243382369d5f7d4465377
codey2015 <35516651+codey2015@users.noreply.github.com>
2018-02-08 17:01:41 -0800

    Update Cube.js

--- Common/Cube.js
+++ Common/Cube.js
@@ -114,8 +114,7 @@ function Cube( vertexShaderId, fragmentShaderId ) {
     this.colors.buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this.colors.buffer);
     gl.bufferData(gl.ARRAY_BUFFER, this.colors.values, gl.STATIC_DRAW);
-    this.colors.attributeLoc = gl.getAttribLocation( this.program, "vColor");
-    gl.enableVertexAttribArray( this.colors.attributeLoc);
+   
 	
     this.indices.buffer = gl.createBuffer();
     gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
@@ -123,6 +122,8 @@ function Cube( vertexShaderId, fragmentShaderId ) {
 	
     this.positions.attributeLoc = gl.getAttribLocation( this.program, "vPosition" );
     gl.enableVertexAttribArray( this.positions.attributeLoc );
+    this.colors.attributeLoc = gl.getAttribLocation( this.program, "vColor");
+    gl.enableVertexAttribArray( this.colors.attributeLoc);
 
     MVLoc = gl.getUniformLocation( this.program, "MV" );
 
