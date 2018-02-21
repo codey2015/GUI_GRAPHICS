1edb1e357af6919fb1b9771b5e11b602c00d6455
codey2015 <35516651+codey2015@users.noreply.github.com>
2018-02-20 19:26:04 -0800

    Update SolarSystem.js

--- Assignment5/SolarSystem.js
+++ Assignment5/SolarSystem.js
@@ -1,42 +1,42 @@
 var SolarSystem = {
   Sun : {
-    radius : 3,  
+    radius : 3.2,  
     distance : 0,
     year : 0,
     color : [ 1.0, 1.0, 0.0, 1.0 ]
   },
   Mercury : {
-    radius : 0.0553,
+    radius : 0.0555,
     distance : 0.387,
     year : 1 / 0.241,
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Venus : {
-    radius : 0.949,
+    radius : 1.0,
     distance : 0.723,
     year : 1 / 0.615,
     color : [ 1.0, 0.0, 1.0, 1.0 ]
   },
   Earth : {
-    radius : 1,
+    radius : 1.15,
     distance : 1,
     year : 1,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
   Moon : {
-    radius : 0.2724,
+    radius : 0.3,
     distance : 0.00257,
     year : 13,
     color : [ 0.5, 0.5, 0.5, 1.0 ]
   },
   Mars : {
-    radius : 0.532,
+    radius : 0.5,
     distance : 1.52,
     year : 1 / 1.88,
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Jupiter : {
-    radius : 11.21,
+    radius : 11.3,
     distance : 5.20,
     year : 1 / 11.9,
     color : [ 1.0, 153/255, 0.0, 1.0 ]
@@ -48,7 +48,7 @@ var SolarSystem = {
     color : [ 1.0, 1.0, 0.0, 1.0 ]
   },
   Uranus : {
-    radius : 4.01,
+    radius : 4.0,
     distance : 19.20,
     year : 1 / 83.7,
     color : [ 0.0, 0.1, 1.0, 1.0 ]
@@ -60,7 +60,7 @@ var SolarSystem = {
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
   Pluto : {
-    radius : 0.186,
+    radius : 0.2,
     distance : 39.48,
     year : 1 / 247.9,
     color : [ 150/255, 75/255, 0.0, 1.0 ]
